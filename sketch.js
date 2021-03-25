fde76cb635896e3ea97c469b4e17e680c5f61085
Stephanie Grasso <sngrasso@dons.usfca.edu>
2021-03-25 02:08:54 -0700

    added some code to sketch.js to make sure library was imported properly

--- sketch.js
+++ sketch.js
@@ -4,4 +4,15 @@ function setup() {
 
 function draw() {
     background(220);
+
+    textAlign(CENTER)
+    drawSprites();
+
+    text("click to test that sprite library is working", width / 2, height / 2);
+}
+
+function mousePressed() {
+    var sprite = createSprite (mouseX, mouseY, 30, 30)
+
+    sprite.velocity.x = 2;
 }
